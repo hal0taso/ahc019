37d7af3822321466ba83dab3744694fb462a1b33
__ukun617 <11371960+hal0taso@users.noreply.github.com>
2023-03-30 04:10:45 +0900

    add script file

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+WORKSPACE="/Users/hal0taso/.ghq/github.com/hal0taso/ahc019"
+cd $WORKSPACE
+g++ main.cpp -std=c++17 -O2 -o main
+
+mkdir -p "${WORKSPACE}/out/"
+mkdir -p "${WORKSPACE}/err/"
+
+for input in `ls ${WORKSPACE}/in`
+do
+echo $input
+"${WORKSPACE}/"main < "${WORKSPACE}/in/${input}" 1> "${WORKSPACE}/out/${input}" 2> "${WORKSPACE}/err/${input}"
+done
